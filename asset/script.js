a451b4aa72d2ac4ea879282fcef4864c4ac73f24
jjiheumi <jimmybaek1212@gmail.com>
2024-12-04 00:15:01 +0900

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -1,8 +1,15 @@
+function wait(sec) {
+    let start = Date.now(), now = start;
+    while (now - start < sec * 1000) {
+        now = Date.now();
+    }
+}
+
 barba.init({
     transitions: [
         {
             name: 'default-transition',
-            leave() {},
+            leave() {wait(0.6)},
             enter() {
                 // create your amazing enter animation here
             }
